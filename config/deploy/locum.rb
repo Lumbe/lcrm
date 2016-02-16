ab8325680f0e0defd10b9bbf7762f9ccf2ac4f02
lumen <just.me.sober@gmail.com>
2016-02-16 14:10:48 +0200

    del capistrano config files

--- config/deploy/locum.rb
+++
@@ -1,6 +0,0 @@
-role :app, %w(hosting_lumen@titanium.locum.ru)
-role :web, %w(hosting_lumen@titanium.locum.ru)
-role :db, %w(hosting_lumen@titanium.locum.ru)
-
-set :ssh_options, forward_agent: true
-set :rails_env, :production
